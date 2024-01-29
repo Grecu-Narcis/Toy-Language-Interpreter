402d95629aa0d8d87ea922abb4014a96b6f5b261
Grecu Narcis <narcisgrecu07@gmail.com>
2024-01-29 22:34:44 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module ubb.ToyLanguage {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ubb to javafx.fxml;
+    exports ubb.models.statements;
+    exports ubb.infrastructure;
+    exports ubb.business;
+    opens ubb.business to javafx.fxml;
+    exports ubb.presentation.gui;
+    opens ubb.presentation.gui to javafx.fxml;
+    exports ubb.business.view_controllers;
+    opens ubb.business.view_controllers to javafx.fxml;
+}
